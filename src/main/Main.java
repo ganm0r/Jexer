d4f2c5878b25d018bbb98a3f3a16bdd549b27b90
ganm0r <gndhrvmv@gmail.com>
2022-04-25 10:32:42 +0530

    :sparkles: Update output file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -25,8 +27,21 @@ public class Main {
     Lexer lexer = new Lexer();
     lexer.tokenize(sourceCodeString);
 
-    List<String> resultingTokenStringsList = lexer.getTokens().stream().map(token -> Objects.toString(token, null)).collect(Collectors.toList());
+    List<String> resultingTokenStringsList = lexer.getFilteredTokens().stream().map(token -> Objects.toString(token, null)).collect(Collectors.toList());
 
-    Files.write(outputFilePath, resultingTokenStringsList, StandardCharsets.US_ASCII);
+    // Files.write(outputFilePath, resultingTokenStringsList, StandardCharsets.US_ASCII);
+
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH));
+
+    int tokenNumber = 0;
+
+    for(String token : resultingTokenStringsList) {
+      tokenNumber++;
+
+      bufferedWriter.write(tokenNumber+" "+token);
+      bufferedWriter.newLine();
+    }
+
+    bufferedWriter.close();
   }
 }
